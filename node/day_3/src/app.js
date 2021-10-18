c7f41bb6580e5df59244c05949371656374cb1c3
one-coding <jo1996kim@gmail.com>
2021-10-18 19:05:29 +0900

    api test in day_3

--- node/day_3/src/app.js
+++ node/day_3/src/app.js
@@ -2,7 +2,8 @@
 
 const express = require('express')
 const userRouter = require('./routers/user')
-
+const axios = require('axios')
+const session = require('express-session')
 const app = express()
 app.use(express.json())
 app.use('/public', express.static('src/public'))
@@ -11,11 +12,42 @@ app.use('/uploads', express.static('uploads'))
 app.set('views', 'src/views')
 app.set('view engine', 'pug')
 
-app.use('/users', userRouter)
-app.use('/', (req, res) => {
-  res.render('index', {
-    message: 'Hello, Pug!!',
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'cookiesecret',
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
   })
+)
+
+app.use('/users', userRouter)
+app.use('/', async (req, res) => {
+  try {
+    console.log('hi')
+    if (!req.session.jwt) {
+      //@ts-ignore
+      const tokenResult = await axios.post('http://localhost:3085/v1/token', {
+        clientSecret: '1f34d111-000f-4fe6-a874-2f59b7c1695e',
+      })
+      console.log(tokenResult.data)
+      if (tokenResult.data && tokenResult.data.code === 200) {
+        //@ts-ignore
+        req.session.jwt = tokenResult
+      } else {
+        return res.json(tokenResult.data)
+      }
+    }
+
+    // res.render('index', {
+    //   message: 'Hello, Pug!!',
+    // })
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 // @ts-ignore
--- node/day_3/src/main.js
+++ node/day_3/src/main.js
@@ -6,6 +6,7 @@ const app = require('./app')
 
 const PORT = 5000
 
+
 app.listen(PORT, () => {
   console.log(`The Express Server listening at ${PORT}`)
 })
